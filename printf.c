3089f298eb05ee493bcda3f50cd20aa1850572e3
AdamTa05 <adamtassaft05@gmail.com>
2023-11-14 17:03:31 -0800

    0

--- printf.c
+++ printf.c
@@ -1,56 +1,48 @@
-/* printf.c */
-#include <stdio.h>
 #include <stdarg.h>
-#include <unistd.h>
+#include <stdio.h>
 #include "main.h"
 
-int _printf(const char *format, ...)
-{
+int _printf(const char *format, ...) {
     va_list args;
     int count = 0;
-    const char *ptr;
 
     va_start(args, format);
 
-    for (ptr = format; ptr && *ptr; ++ptr)
-    {
-        if (*ptr == '%' && ptr[1] != '\0')
-        {
-            /* Handle conversion specifier */
-            ++ptr; /* Move to the character after '%' */
-            switch (*ptr)
-            {
-                case 'c':
-                    putchar(va_arg(args, int));
+    while (*format != '\0') {
+        if (*format == '%') {
+            format++; /* Move to the next character after % */
+
+            switch (*format) {
+                case 'c': {
+                    char ch = (char)va_arg(args, int);
+                    putchar(ch);
                     count++;
                     break;
-                case 's':
-                    count += write(1, va_arg(args, char *), _strlen(va_arg(args, char *)));
+                }
+                case 's': {
+                    char *str = va_arg(args, char *);
+                    while (*str != '\0') {
+                        putchar(*str);
+                        str++;
+                        count++;
+                    }
                     break;
+                }
                 case '%':
-                    count += write(1, "%", 1);
+                    putchar('%');
+                    count++;
                     break;
                 default:
-                    count += write(1, &(*ptr), 1);
                     break;
             }
+        } else {
+            putchar(*format);
+            count++;
         }
-        else
-        {
-            /* Regular character, just print it */
-            count += write(1, &(*ptr), 1);
-        }
+
+        format++;
     }
 
     va_end(args);
-
     return count;
-}
-
-int _strlen(const char *str)
-{
-    int len = 0;
-    while (str[len])
-        len++;
-    return len;
 }
